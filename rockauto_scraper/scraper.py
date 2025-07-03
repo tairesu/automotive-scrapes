2ea5b9171176fdd2c930d88c4250ecd414ae0ec0
tairesu <tyresecook25@yahoo.com>
2025-07-03 16:04:34 -0500

    Two means of scraping: While & Recursive

--- rockauto_scraper/scraper.py
+++ rockauto_scraper/scraper.py
@@ -1,17 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 
-
-headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36"
-}
-
-
-models = []
-trims = []
-categories  = []
-# Return appropriate URL based on parameters
-def handle_url(make, year, model=None, trim=None, category=None, part=None):
+# Return appropriate Rockauto URL based on parameters
+def get_rockauto_url(make, year, model=None, trim=None, category=None, part=None):
     if not model:
         url = f'https://www.rockauto.com/en/catalog/{make},{year}'
     elif not trim:
@@ -25,57 +16,78 @@ def handle_url(make, year, model=None, trim=None, category=None, part=None):
     return url
 
 
-def selection(openLinksList) :
-    
-    for (i,anchor) in enumerate(openLinksList):
+def make_decision(opts) :
+    for (i,anchor) in enumerate(opts):
         print(i,anchor['name'])
-    selection = int(input("Make a selection"))    
-    return openLinksList[selection]
 
-# Call next recursive case based on parameters
-def handle_next_recursion(openLinksList, make, year, model=None, trim=None, category=None, part=None):
-    if not model:
-        # Go deeper with model
-        
-        return get_rockauto_data(make, year, model=selection(openLinksList))
-    elif not trim:
-        print(f'\n\nall trims: \n {openLinksList} \n')
-        # Go deeper with trim
-        return get_rockauto_data(make, year, model, trim=openLinksList[0])
-    elif not category:
-        print(f'\n\nall categories: \n {openLinksList} \n')
-        categories = openLinksList
-        # Go deeper with category
-        return get_rockauto_data(make, year, model, trim, openLinksList[0])
-    elif not part:
-        print(f'\n\nall parts: \n {openLinksList} \n')
-        categories = openLinksList
-        # Go deeper with category
-        return get_rockauto_data(make, year, model, trim, openLinksList[0])
+    choice = int(input("Make a selection"))    
+    return opts[choice]
 
-
-def get_rockauto_data(make, year, model=None, trim=None, category=None, part=None):
-    # Build URL based on state
-    url = handle_url(make,year,model,trim,category,part )
-
-    print(f'FETCHING: {url}\n')
+# Returns Code from a rockauto url
+def get_page_html(url):
+    headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36"
+    }
     html = requests.get(url, headers=headers).text
     soup = BeautifulSoup(html, 'html.parser')
+    return soup
 
-    # BASE CASE: table with parts is loaded
-    if soup.find('tbody', class_='listing-inner'):
-        partsTbodyData = soup.find('tbody', class_='listing-inner')
-        print(f"💥 Found {len(partsTbodyData)} Parts")
-        return 
-
+# Checks rockauto url for parts table
+def is_parts_present(url):
+    # Grabs page HTML
+    soup = get_page_html(url)
+    # Rockauto Parts Tables
+    partsTables = soup.find('tbody', class_='listing-inner')
+    # Table with parts is loaded
+    if partsTables:
+        print(f"💥 Found {len(partsTables)} Parts")
+        return True 
+    return False
+    
+# Return the list of expanded rockauto items    
+def grab_rockauto_links(url):
     # RECURSIVE CASES
+    soup = get_page_html(url)
     expandedIcons = soup.find_all('td', class_='nexpandedicon')
     activeTd = expandedIcons[-1]
     activeListDiv = activeTd.find_parent('div', class_='ranavnode')
     activeListAnchors = activeListDiv.find_all('a', class_=["navlabellink","nvoffset","nnormal"])[1:]
     openLinksList = [{'name': a.text.strip(), 'slug': a['href']} for a in activeListAnchors]
+    return openLinksList
 
-    handle_next_recursion(openLinksList, make, year, model=None, trim=None, category=None, part=None)
+
+def get_url_postselection(choice,make,year,model=None,trim=None,category=None,part=None):
+    if not model:
+        url = get_rockauto_url(make,year,model=choice)
+    elif not trim:
+        url = get_rockauto_url(make,year,model,trim=choice)
+    elif not category:
+        url = get_rockauto_url(make,year,model,trim,category=choice)
+    elif not part:
+        url = get_rockauto_url(make,year,model,trim,category,part=choice)
+
+    return url
+
+
+def get_rockauto_data(make, year, model=None, trim=None, category=None, part=None):
+    # Build URL based on state
+    url = get_rockauto_url(make,year,model,trim,category,part)
+    return grab_rockauto_links(url)
+
     
-    
-print(get_rockauto_data('honda','2004'))
+if __name__ == '__main__':
+    make = input('What make?: ')
+    year = input('What year?: ')
+    model = {}
+    trim = {}
+    category = {}
+    part = {}
+
+    url = get_rockauto_url(make,year)
+    while (not is_parts_present(url)):
+        rockautoListItems = grab_rockauto_links(url)
+        choice = make_decision(rockautoListItems)
+        print("You selected: \n", choice)
+        
+        #Get new Url from ListItem selection
+        url = get_url_postselection(choice,make,year,model,trim,category,part)
